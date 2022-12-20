7cc2290bd074450c1b6eb0ddd1b9a4fbc4117c00
nprvulovic <nikola.prvulovic@ventum.com>
2022-12-20 10:43:34 +0100

    Test Programm for list and inputs

--- lego-ui/main.go
+++ lego-ui/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fyne.io/fyne/v2/layout"
 	"image/color"
+	"log"
 	"net/http"
 	"time"
 
@@ -10,8 +12,6 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -38,14 +38,16 @@ func getRandomFact() (randomFact, error) {
 	return fact, nil
 }
 
+var data = []string{"Cert1", "Cert2", "Cert3"}
+
 func main() {
 	client = &http.Client{Timeout: 10 * time.Second}
 
 	a := app.New()
-	win := a.NewWindow("Get Useless Fact")
-	win.Resize(fyne.NewSize(800, 300))
+	win := a.NewWindow("LEGO-UI")
+	win.Resize(fyne.NewSize(1000, 1000))
 
-	title := canvas.NewText("Get Your Useless Facts", color.White)
+	title := canvas.NewText("Get Your TLS Certificates", color.White)
 	title.TextStyle = fyne.TextStyle{
 		Bold: true,
 	}
@@ -55,18 +57,69 @@ func main() {
 	factText := widget.NewLabel("")
 	factText.Wrapping = fyne.TextWrapWord
 
-	button := widget.NewButton("Get Fact", func() {
-		fact, err := getRandomFact()
-		if err != nil {
-			dialog.ShowError(err, win)
-		} else {
-			factText.SetText(fact.Text)
-		}
+	customerNumber := canvas.NewText("Customer Number", color.White)
+	customerNumber.Alignment = fyne.TextAlignLeading
+
+	inputCustomerNumber := widget.NewEntry()
+	inputCustomerNumber.SetPlaceHolder("Enter text...")
+
+	apiKey := canvas.NewText("API Key", color.White)
+	apiKey.Alignment = fyne.TextAlignLeading
+
+	inputAPIKey := widget.NewEntry()
+	inputAPIKey.SetPlaceHolder("Enter text...")
+
+	apiPass := canvas.NewText("Api Password", color.White)
+	apiPass.Alignment = fyne.TextAlignLeading
+
+	inputAPIPass := widget.NewPasswordEntry()
+	inputAPIPass.SetPlaceHolder("Enter text...")
+
+	logOut := canvas.NewText("Logging Output", color.White)
+	logOut.Alignment = fyne.TextAlignLeading
+
+	logOutBox := widget.NewMultiLineEntry()
+
+	logOutBox.SetPlaceHolder("Hier sind die logs lines " + "\n" +
+		"Lgoing Data Testoutput" + "\n" + "Lgoing Data Testoutput")
+
+	logOutBox.Resize(fyne.Size{400, 1000})
+	logOutBox.Move(fyne.Position{400, 0})
+
+	getCertButton := widget.NewButton("GetCert", func() {
+		log.Println("Content was:", inputCustomerNumber.Text)
 	})
 
-	hBox := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button, layout.NewSpacer())
-	vBox := container.New(layout.NewVBoxLayout(), title, hBox, widget.NewSeparator(), factText)
+	list := widget.NewList(
+		func() int {
+			return len(data)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i])
+		})
+	list.Resize(fyne.Size{200, 1000})
+	list.Move(fyne.Position{0, 0})
+	//inBox := container.New(layout.NewCenterLayout(), layout.NewSpacer(), inputCustomerNumber, layout.NewSpacer(), saveButton, layout.NewSpacer())
 
-	win.SetContent(vBox)
+	//hBox := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), button, layout.NewSpacer())
+	//vBox := container.New(layout.NewVBoxLayout(), title, hBox, widget.NewSeparator(), inBox, widget.NewSeparator(), factText)
+	//test := container.NewContainerW
+
+	testbox := container.New(layout.NewVBoxLayout(), title)
+	testbox2 := container.New(layout.NewVBoxLayout(), testbox, customerNumber, inputCustomerNumber, apiKey, inputAPIKey, apiPass, inputAPIPass, getCertButton)
+	testbox4 := container.NewWithoutLayout(logOutBox)
+	//testbox7 := container.NewWithoutLayout(list)
+
+	//testbox5 := container.New(layout.NewHBoxLayout(), testbox7, widget.NewSeparator(), testbox2, testbox4)
+	testbox6 := container.New(layout.NewHBoxLayout(), testbox4, testbox2)
+	testbox7 := container.New(layout.NewHBoxLayout(), testbox4, testbox2)
+	tabItem1 := container.NewTabItem("DNS1", testbox6)
+	tabItem2 := container.NewTabItem("DNS2", testbox7)
+	tabs := container.NewAppTabs(tabItem1, tabItem2)
+	win.SetContent(tabs)
+
 	win.ShowAndRun()
 }
